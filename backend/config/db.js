3c926e5df36d85f2770bf6ee243a9892d1adf6a7
macdogiahuy <macdogiahuy123@gmail.com>
2024-10-19 18:48:24 +0700

    update gitignore, back-end

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
-const {sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASS,
   {
-    host: 'process.env.DB_HOST',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
   }
 );
