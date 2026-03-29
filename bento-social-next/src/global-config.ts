2046c032eecec3f730ef03408141b3092faffcd6
macdogiahuy <macdogiahuy123@gmail.com>
2026-03-29 13:58:00 +0700

    Stabilize production socket connection

--- bento-social-next/src/global-config.ts
+++ bento-social-next/src/global-config.ts
@@ -7,3 +7,8 @@ export const MEDIA_HOST_API =
   process.env.NEXT_PUBLIC_MEDIA_URL ||
   process.env.NEXT_PUBLIC_CDN_URL ||
   HOST_API;
+
+export const SOCKET_HOST_API =
+  process.env.NEXT_PUBLIC_SOCKET_URL ||
+  process.env.NEXT_PUBLIC_WS_URL ||
+  HOST_API;
